aefce8c7cd30f057b2fd49f9ea92de84b663430a
l08084 <l08084@shibaura-it.ac.jp>
2015-11-13 15:24:53 +0900

    Initial Commit

---
+++ Sodateru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sodateru
+//
+//  Created by 中安拓也 on 2015/11/13.
+//  Copyright © 2015年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
